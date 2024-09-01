dba187553e0e8e7292daa2c4b0161fc1fcf9e616
insearching <graser1305@gmail.com>
2024-09-01 03:20:35 +0200

    Fixed main file, update libraries.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:portfolio/portfolio_app.dart';
+import 'package:portfolio/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
