b00b3cffd8c860e0eb4e3f395754da5be6ca2613
insearching <graser1305@gmail.com>
2024-10-14 22:53:02 +0200

    Fixed progress of skills.

--- lib/main/ui/components/skill_progress_bar.dart
+++ lib/main/ui/components/skill_progress_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/utils/colors.dart';
+import 'package:portfolio/utils/measure_size.dart';
 
 class SkillProgressBar extends StatefulWidget {
   const SkillProgressBar({
@@ -23,69 +24,75 @@ class _SkillProgressBarState extends State<SkillProgressBar> {
   OverlayEntry? sticky;
   GlobalKey stickyKey = GlobalKey();
 
+  Size progressBarSize = const Size(0, 0);
+
   @override
   Widget build(BuildContext context) {
-    // var childSize = const Size.fromWidth(200);
-    final percentProgress = widget.progress / 100;
-    final percentString = '${(percentProgress * 100).toStringAsFixed(0)}%';
-    // final percentageWidth = childSize.width / 100 * widget.progress;
+    final percentString = '${widget.progress.toStringAsFixed(0)}%';
+
     const height = 8.0;
     const radius = 3.0;
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              widget.title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16.0,
-              ),
-            ),
-            Text(
-              percentString,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16.0,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 8.0),
-        Stack(
-          children: [
-            Container(
-              // width: childSize.width,
-              height: height,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [UIColors.black, UIColors.backgroundColorLight],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomLeft,
+    return MeasureSize(
+      onChange: (Size size) {
+        setState(() {
+          progressBarSize = size;
+        });
+      },
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
                 ),
-                borderRadius: BorderRadius.circular(radius),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Container(
-                // width: percentageWidth,
+              Text(
+                percentString,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8.0),
+          Stack(
+            children: [
+              Container(
                 height: height,
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [widget.startColor, widget.endColor],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
+                  gradient: const LinearGradient(
+                    colors: [UIColors.black, UIColors.backgroundColorLight],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomLeft,
                   ),
                   borderRadius: BorderRadius.circular(radius),
                 ),
               ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 24.0)
-      ],
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Container(
+                  width: progressBarSize.width / 100 * widget.progress,
+                  height: height,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [widget.startColor, widget.endColor],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                    borderRadius: BorderRadius.circular(radius),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 24.0)
+        ],
+      ),
     );
   }
 }
